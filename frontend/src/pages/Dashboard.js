493f30493f7321c19645613c2ee450c360981b4b
Oliver Rikkilä <oliver.rikkila@siili.com>
2020-05-27 10:22:07 +0300

    pie chart example d3

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,7 +1,39 @@
-﻿import React from 'react';
+﻿import React, { useState } from 'react';
+import * as d3 from 'd3';
+import Pie from '../components/Pie';
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const generateData = (value, length = 5) =>
+        d3.range(length).map((item, index) => ({
+            date: index,
+            value:
+                value === null || value === undefined
+                    ? Math.random() * 100
+                    : value
+        }));
+
+    const [data, setData] = useState(generateData());
+    const changeData = () => {
+        setData(generateData());
+    };
+
+    return (
+        <div>
+            <div>
+                <button onClick={changeData}>Transform</button>
+            </div>
+            <div>
+                <span className="label">Hooks</span>
+                <Pie
+                    data={data}
+                    width={200}
+                    height={200}
+                    innerRadius={60}
+                    outerRadius={100}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default Dashboard;
